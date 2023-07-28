18921ebe2544912ebb4f573037cbcece2a0683d1
WisyaHD <wisyahariadarma@gmail.com>
2023-07-28 17:42:24 +0700

    feat(major): Adding New Testing Hancur Barang and Adding New Form Modal Otorisasi

--- cypress/e2e/page-objects/Page/BarangPage/BarangPage.ts
+++ cypress/e2e/page-objects/Page/BarangPage/BarangPage.ts
@@ -1,12 +1,15 @@
 import DataBarangPage from "./DataBarangPage/DataBarangPage"
+import HancurBarangPage from "./HancurBarangPage/HancurBarangPage";
 import TambahBarangPage from "./TambahBarangPage/TambahBarangPage";
 
 export default class BarangPage{
     public dataBarangPage: DataBarangPage;
     public tambahBarangPage: TambahBarangPage;
+    public hancurBarangPage: HancurBarangPage;
 
     constructor(){
         this.dataBarangPage = new DataBarangPage();
         this.tambahBarangPage = new TambahBarangPage();
+        this.hancurBarangPage = new HancurBarangPage();
     }
 }
--- cypress/e2e/page-objects/Page/Page.ts
+++ cypress/e2e/page-objects/Page/Page.ts
@@ -1,15 +1,20 @@
 import BarangPage from "./BarangPage/BarangPage";
 import LaporanPage from "./LaporanPage/LaporanPage";
+import ModalOtorisasi from "./ModalOtorisasi/ModalOtorisasi";
 import PenjualanPage from "./PenjualanPage/PenjualanPage";
 
 export default class Page{
     public barangPage: BarangPage;
     public penjualanPage: PenjualanPage;
-    public laporanPage: LaporanPage
+    public laporanPage: LaporanPage;
+
+    // Modal
+    public modalOtorisasi: ModalOtorisasi;
 
     constructor(){
         this.barangPage = new BarangPage();
         this.penjualanPage = new PenjualanPage();
         this.laporanPage = new LaporanPage();
+        this.modalOtorisasi = new ModalOtorisasi();
     }
 }
